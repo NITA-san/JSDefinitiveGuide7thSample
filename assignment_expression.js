ce8d61fa83966ab7920c7be8f3e4a8ec5b4539ad
NITA-san <ysnitta@gmail.com>
2020-10-15 13:48:15 +0900

    Expression sample codes

---
+++ assignment_expression.js
@@ -0,0 +1,7 @@
+let i = 0;
+let data = [1,2,3,1,1,1,1,1,1,1]
+
+
+data[i++] *= 2;
+//data[i++] = data[i++] * 2;
+console.log(data);
---
+++ evaluation_expression.js
@@ -0,0 +1,14 @@
+const geval = eval;               // Using another name does a global eval
+let x = "global", y = "global";   // Two global variables
+function f() {                    // This function does a local eval
+    let x = "local";              // Define a local variable
+    eval("x += 'changed';");      // Direct eval sets local variable
+    return x;                     // Return changed local variable
+}
+function g() {                    // This function does a global eval
+    let y = "local";              // A local variable
+    geval("y += 'changed';");     // Indirect eval sets global variable
+    return y;                     // Return unchanged local variable
+}
+console.log(f(), x); // Local variable changed: prints "localchanged global":
+console.log(g(), y); // Global variable changed: prints "local globalchanged":
