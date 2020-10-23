f58fe0120261b7a69a43550c4834e4bdfd06155f
NITA-san <ysnitta@gmail.com>
2020-10-23 11:54:10 +0900

    Try use strict behavior about functions

--- statement/use_strict.js
+++ statement/use_strict.js
@@ -1,17 +1,29 @@
-//'use strict';
+'use strict';
 function test(a) {
   console.log(a);
   console.log(this);
 }
 test(10);
 
+/*
 b = 20
 console.log(b);
 
-/*
 with (true) {
   console.log(1)
 };
 */
 
-//console.log(nondeclaredvar);
+let aa;
+eval("let aa=3;console.log(aa);");
+console.log(aa);
+//eval("let aa=2; console.log(aa);");
+
+let o = {a:1,a:2}
+console.log(o.a);
+
+function f(a,a) {
+  console.log(a);
+}
+
+f(1,100);
