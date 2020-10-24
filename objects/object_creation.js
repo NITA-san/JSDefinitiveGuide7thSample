0eb4e827df7ea4b5ed630f9d9f2f2fda028e911d
NITA-san <ysnitta@gmail.com>
2020-10-24 09:42:26 +0900

    Try object creation behavior

---
+++ objects/object_creation.js
@@ -0,0 +1,6 @@
+'use strict';
+let o3 = Object.create(Object.prototype);
+
+let o = { x: "don't change this value" };
+//library.function(Object.create(o));  // Guard against accidental modifications
+
