d2e872a9c1a0b9e5b15ed5dd30a81a92c2d4e6e5
NITA-san <ysnitta@gmail.com>
2020-11-02 23:11:39 +0900

    Try promise behavior

--- async/test_promise.js
+++ async/test_promise.js
@@ -1,22 +1,38 @@
 // Suppose you have a function like this to display a user profile
 function displayUserProfile(profile) { /* implementation omitted */
+    console.log(profile);
     console.log("display")
 }
 
-function displayError(){
+function displayError(s) {
+    console.log(s);
     console.log("error")
 }
 
 function getJSON(url) {
     return new Promise((resolve, reject) => {
-        console.log("in promise")
-//        resolve();
-        reject();
-     });
+        //        console.log("in promise")
+        //        resolve();
+        setTimeout(() => resolve("promise rejected! " + url), 2000);
+        //        resolve();
+    });
 }
 
 // Here's how you might use that function with a Promise.
 // Notice how this line of code reads almost like an English sentence:
 getJSON("/api/user/profile").then(displayUserProfile).catch(displayError);
 
+function createPromise(url) {
+
+    //    return new Promise((resolve) => resolve("promise rejected! " + url));
+    setTimeout(() => {return "promise rejected! " + url;}, 2000);
+}
+
+async function getJSONAsync(url) {
+    p = createPromise(url);
+    //    p = await getJSON(url);
+    displayUserProfile(p);
+}
+
+getJSONAsync("/api/user/profile")
 
