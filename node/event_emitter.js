6de58721dc7809f8d13c062f47b2013f42a3ccb9
NITA-san <ysnitta@gmail.com>
2020-11-09 22:47:58 +0900

    Try evnet emitter behavior

--- node/event_emitter.js
+++ node/event_emitter.js
@@ -1,5 +1,14 @@
-const EventEmitter = require("events"); // Module name does not match class name
+// const EventEmitter = require("events"); // Module name does not match class name
+// const net = require("net");
+// let server = new net.Server();          // create a Server object
+
+// console.log(server instanceof EventEmitter);          // => true: Servers are EventEmitters
+
 const net = require("net");
 let server = new net.Server();          // create a Server object
-
-console.log(server instanceof EventEmitter);          // => true: Servers are EventEmitters
+server.on("connection", socket => {     // Listen for "connection" events
+    // Server "connection" events are passed a socket object
+    // for the client that just connected. Here we send some data
+    // to the client and disconnect.
+    socket.end("Hello World", "utf8");
+});
