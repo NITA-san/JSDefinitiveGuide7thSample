56a28dfc1ca3e83506ea257fcef917de4647ee4c
NITA-san <ysnitta@gmail.com>
2020-11-10 22:49:06 +0900

    Try stream behavior

---
+++ node/streams.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+// An asynchronous but nonstreaming (and therefore inefficient) function.
+function copyFile(sourceFilename, destinationFilename, callback) {
+    fs.readFile(sourceFilename, (err, buffer) => {
+        if (err) {
+            callback(err);
+        } else {
+            fs.writeFile(destinationFilename, buffer, callback);
+        }
+    });
+}
+
+callback = ()=>{console.log("yeah!")};
+copyFile("test.json","out.json", callback);
