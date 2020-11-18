eb3bd5cbf41471a99af3547f587b1597be38910a
NITA-san <ysnitta@gmail.com>
2020-11-18 22:37:03 +0900

    Try path object

---
+++ node/path.js
@@ -0,0 +1,35 @@
+
+const os = require("os");
+// Some important paths
+process.cwd()      // Absolute path of the current working directory.
+__filename         // Absolute path of the file that holds the current code.
+__dirname          // Absolute path of the directory that holds __filename.
+os.homedir()       // The user's home directory.
+
+const path = require("path");
+
+path.sep                         // Either "/" or "\" depending on your OS
+
+// The path module has simple parsing functions
+let p = "src/pkg/test.js";       // An example path
+console.log(path.basename(p));                 // => "test.js"
+console.log(path.extname(p));                  // => ".js"
+console.log(path.dirname(p));                  // => "src/pkg"
+console.log(path.basename(path.dirname(p)));   // => "pkg"
+console.log(path.dirname(path.dirname(p)));    // => "src"
+
+// normalize() cleans up paths:
+path.normalize("a/b/c/../d/")    // => "a/b/d/": handles ../ segments
+path.normalize("a/./b")          // => "a/b": strips "./" segments
+path.normalize("//a//b//")       // => "/a/b/": removes duplicate /
+
+// join() combines path segments, adding separators, then normalizes
+path.join("src", "pkg", "t.js")  // => "src/pkg/t.js"
+
+// resolve() takes one or more path segments and returns an absolute
+// path. It starts with the last argument and works backward, stopping
+// when it has built an absolute path or resolving against process.cwd().
+path.resolve()                   // => process.cwd()
+path.resolve("t.js")             // => path.join(process.cwd(), "t.js")
+path.resolve("/tmp", "t.js")     // => "/tmp/t.js"
+path.resolve("/a", "/b", "t.js") // => "/b/t.js"
