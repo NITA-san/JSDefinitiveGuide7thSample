89139d0fbb7054b8dc8f136123427ed7e57dbca2
NITA-san <ysnitta@gmail.com>
2020-11-19 23:02:34 +0900

    Try path behavior

--- node/path.js
+++ node/path.js
@@ -19,17 +19,17 @@ console.log(path.basename(path.dirname(p)));   // => "pkg"
 console.log(path.dirname(path.dirname(p)));    // => "src"
 
 // normalize() cleans up paths:
-path.normalize("a/b/c/../d/")    // => "a/b/d/": handles ../ segments
-path.normalize("a/./b")          // => "a/b": strips "./" segments
-path.normalize("//a//b//")       // => "/a/b/": removes duplicate /
+console.log(path.normalize("a/b/c/../d/"));    // => "a/b/d/": handles ../ segments
+console.log(path.normalize("a/./b"));          // => "a/b": strips "./" segments
+console.log(path.normalize("//a//b//"));       // => "/a/b/": removes duplicate /
 
 // join() combines path segments, adding separators, then normalizes
-path.join("src", "pkg", "t.js")  // => "src/pkg/t.js"
+console.log(path.join("src", "pkg", "t.js"));  // => "src/pkg/t.js"
 
 // resolve() takes one or more path segments and returns an absolute
 // path. It starts with the last argument and works backward, stopping
 // when it has built an absolute path or resolving against process.cwd().
-path.resolve()                   // => process.cwd()
-path.resolve("t.js")             // => path.join(process.cwd(), "t.js")
-path.resolve("/tmp", "t.js")     // => "/tmp/t.js"
-path.resolve("/a", "/b", "t.js") // => "/b/t.js"
+console.log(path.resolve());                   // => process.cwd()
+console.log(path.resolve("t.js"));             // => path.join(process.cwd(), "t.js")
+console.log(path.resolve("/tmp", "t.js"));     // => "/tmp/t.js"
+console.log(path.resolve("/a", "/b", "t.js")); // => "/b/t.js"
