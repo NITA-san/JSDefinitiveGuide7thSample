74eb5999080722e1faab30409458b9887a80eb3d
NITA-san <ysnitta@gmail.com>
2020-11-27 22:07:11 +0900

    Try unlink promise's version

--- node/rename_file.js
+++ node/rename_file.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 
 fs.copyFileSync("ch15.txt", "ch15.bak");
 fs.renameSync("ch15.bak", "ch16.bak");
-fs.unlinkSync("ch16.bak");
+fs.promises.unlink("ch16.bak").then(console.log("unlinked"));
