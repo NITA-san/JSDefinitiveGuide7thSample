5f05479701e993f6c7d2112a9e826d17853440be
NITA-san <ysnitta@gmail.com>
2020-12-05 21:18:29 +0900

    Try child process fork

--- node/child_process_spawn.js
+++ node/child_process_spawn.js
@@ -1,4 +1,4 @@
 const child_process = require("child_process");
 
-let process = child_process.spawn("ls",["-l"]);
+let process = child_process.spawn("ls",["-al"]);
 process.stdout.on("data", (data) => console.log(data.toString()));
