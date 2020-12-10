0b6fe07b5f3046fe1cf839b533565457d26a2ff0
NITA-san <ysnitta@gmail.com>
2020-12-10 22:19:24 +0900

    Try pseudo array

---
+++ arrays/pseudo_array.js
@@ -0,0 +1,18 @@
+let a = {};  // Start with a regular empty object
+
+// Add properties to make it "array-like"
+let i = 0;
+while(i < 10) {
+    a[i] = i * i;
+    i++;
+}
+a.length = i;
+
+// Now iterate through it as if it were a real array
+let total = 0;
+for(let j = 0; j < a.length; j++) {
+    total += a[j];
+}
+
+console.log(a.length);
+console.log(a[3]);
