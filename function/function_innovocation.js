692613be73126852f99a9d3b45947afcace6a4fb
NITA-san <ysnitta@gmail.com>
2020-12-12 23:23:26 +0900

    Try function innovacation.js

---
+++ function/function_innovocation.js
@@ -0,0 +1,7 @@
+//printprops({x: 1});
+//let total = distance(0,0,2,1) + distance(2,1,3,5);
+//let probability = factorial(5)/factorial(13);
+
+const f = (x) => 2 * x;
+const x = 4;
+console.log((f !== null && f !== undefined) ? f(x) : undefined);
