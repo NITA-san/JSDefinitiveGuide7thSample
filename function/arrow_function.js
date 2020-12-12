21395ddc1cd2ffd6f402c73ffe69691a140830cb
NITA-san <ysnitta@gmail.com>
2020-12-12 23:08:52 +0900

    Try arrow function

---
+++ function/arrow_function.js
@@ -0,0 +1,7 @@
+// Make a copy of an array with null elements removed.
+let filtered = [1,null,2,3].filter(x => x !== null); // filtered == [1,2,3]
+// Square some numbers:
+let squares = [1,2,3,4].map(x => x*x);               // squares == [1,4,9,16]
+
+console.log(filtered);
+console.log(squares);
