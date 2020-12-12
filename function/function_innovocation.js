9ce7a88b31de40924ab1c7d929184e14c6e91d99
NITA-san <ysnitta@gmail.com>
2020-12-12 23:55:17 +0900

    Try detect strict mode

--- function/function_innovocation.js
+++ function/function_innovocation.js
@@ -1,3 +1,4 @@
+//"use strict"
 //printprops({x: 1});
 //let total = distance(0,0,2,1) + distance(2,1,3,5);
 //let probability = factorial(5)/factorial(13);
@@ -5,3 +6,10 @@
 const f = (x) => 2 * x;
 const x = 4;
 console.log((f !== null && f !== undefined) ? f(x) : undefined);
+
+// Define and invoke a function to determine if we're in strict mode.
+const strict = (function() { 
+    console.log(this);
+    return !this; 
+}());
+console.log(strict);
