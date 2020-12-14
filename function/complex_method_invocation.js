3da83e799324010cac286ec76f53ea6a804feb37
NITA-san <ysnitta@gmail.com>
2020-12-14 22:00:07 +0900

    Try complex method invocation

--- function/complex_method_invocation.js
+++ function/complex_method_invocation.js
@@ -12,3 +12,17 @@ f().m();                        // Invoke method m() on return value of f()
 
 console.log(f().m());
 console.log(customer.surname.toUpperCase());
+
+let o = {                 // An object o.
+    m: function() {       // Method m of the object.
+        let self = this;  // Save the "this" value in a variable.
+        console.log(this === o);        // => true: "this" is the object o.
+        f();              // Now call the helper function f().
+
+        function f() {    // A nested function f
+            console.log(this === o);    // => false: "this" is global or undefined
+            console.log(self === o);    // => true: self is the outer "this" value.
+        }
+    }
+};
+console.log(o.m());                    // Invoke the method m on the object o.
