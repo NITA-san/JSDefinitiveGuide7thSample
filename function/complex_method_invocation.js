6d864a1bd409336fa6756fe4359e709e27c0fd20
NITA-san <ysnitta@gmail.com>
2020-12-14 22:04:23 +0900

    Try complex method invacation (inherit this by arrow function)

--- function/complex_method_invocation.js
+++ function/complex_method_invocation.js
@@ -17,12 +17,16 @@ let o = {                 // An object o.
     m: function() {       // Method m of the object.
         let self = this;  // Save the "this" value in a variable.
         console.log(this === o);        // => true: "this" is the object o.
+        const f = () => {
+            console.log(this === o);  // true, since arrow functions inherit this
+        };
         f();              // Now call the helper function f().
 
-        function f() {    // A nested function f
-            console.log(this === o);    // => false: "this" is global or undefined
-            console.log(self === o);    // => true: self is the outer "this" value.
-        }
+        // function f() {    // A nested function f
+        //     console.log(this === o);    // => false: "this" is global or undefined
+        //     console.log(self === o);    // => true: self is the outer "this" value.
+        // }
+
     }
 };
 console.log(o.m());                    // Invoke the method m on the object o.
