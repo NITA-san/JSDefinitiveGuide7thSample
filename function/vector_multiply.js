01a2d81b58100d3d7b421bd187dbb080d73f3c27
NITA-san <ysnitta@gmail.com>
2020-12-19 14:28:11 +0900

    Try vector multiply with object destructure

--- function/vector_multiply.js
+++ function/vector_multiply.js
@@ -10,3 +10,9 @@ function vectorMultiply2({ x, y, z = 0 }, scalar) {
 }
 console.log(vectorMultiply2({ x: 1, y: 2 }, 2));  // => {x: 2, y: 4, z: 0}
 console.log(vectorMultiply2({ a: 1, b: 2, c: 3 }, 2));  // => {x: 2, y: 4, z: 0}
+
+// Multiply the vector {x,y} or {x,y,z} by a scalar value, retain other props
+function vectorMultiply3({x, y, z=0, ...props}, scalar) {
+    return { x: x*scalar, y: y*scalar, z: z*scalar, ...props };
+}
+console.log(vectorMultiply3({x: 1, y: 2, w: -1}, 2));  // => {x: 2, y: 4, z: 0, w: -1}
