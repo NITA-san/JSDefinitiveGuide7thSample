10ee4f88268f3550044912a49b462815409f59cb
NITA-san <ysnitta@gmail.com>
2020-12-20 22:57:22 +0900

    Try apply another behavior

--- function/apply_function.js
+++ function/apply_function.js
@@ -3,3 +3,7 @@ let f = () => { return "I'm f"};
 const o = {};
 console.log(f.call(o,"test"));
 console.log(f.apply(o));
+
+arrayOfNumbers = [1,2,3,4,5]
+let biggest = Math.max.apply(Math, arrayOfNumbers);
+console.log(biggest);
