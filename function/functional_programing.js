284985e29443513310e3621abee3be3bd92db051
NITA-san <ysnitta@gmail.com>
2020-12-20 23:56:44 +0900

    Try functional programing style

---
+++ function/functional_programing.js
@@ -0,0 +1,12 @@
+const map = function(a, ...args) { return a.map(...args); };
+const reduce = function(a, ...args) { return a.reduce(...args); };
+const sum = (x,y) => x+y;
+const square = x => x*x;
+
+let data = [1,1,3,5,5];
+let mean = reduce(data, sum)/data.length;
+let deviations = map(data, x => x-mean);
+let stddev = Math.sqrt(reduce(map(deviations, square), sum)/(data.length-1));
+console.log(stddev);  // => 2
+
+console.log([1,2,3].map(x => x*x).reduce((x,y)=>x+y));
