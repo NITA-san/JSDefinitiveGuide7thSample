fd95f068fad79aece2197bfdf8ee89f4295dca4f
NITA-san <ysnitta@gmail.com>
2020-12-20 21:21:44 +0900

    Try closure sample

---
+++ function/closure_sample.js
@@ -0,0 +1,7 @@
+let scope = "global scope";          // A global variable
+function checkscope() {
+    let scope = "local scope";       // A local variable
+    function f() { return scope; }   // Return the value in scope here
+    return f();
+}
+console.log(checkscope());                         // => "local scope"
---
+++ function/closure_sample2.js
@@ -0,0 +1,8 @@
+let scope = "global scope";          // A global variable
+function checkscope() {
+    let scope = "local scope";       // A local variable
+    function f() { return scope; }   // Return the value in scope here
+    return f;
+}
+let s = checkscope()();              // What does this return?
+console.log(s);
