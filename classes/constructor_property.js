d65d8054cd2a927373731cdf7973914ea68932bf
NITA-san <ysnitta@gmail.com>
2020-12-29 14:19:03 +0900

    Try constructor property

---
+++ classes/constructor_property.js
@@ -0,0 +1,4 @@
+let F = function() {}; // This is a function object.
+let p = F.prototype;   // This is the prototype object associated with F.
+let c = p.constructor; // This is the function associated with the prototype.
+console.log(c === F);                // => true: F.prototype.constructor === F for any F
