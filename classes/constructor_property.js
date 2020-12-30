e58b1bc250adf855b0478f5132c2010b145c4839
NITA-san <ysnitta@gmail.com>
2020-12-30 23:48:33 +0900

    Check constructor property

--- classes/constructor_property.js
+++ classes/constructor_property.js
@@ -2,3 +2,6 @@ let F = function() {}; // This is a function object.
 let p = F.prototype;   // This is the prototype object associated with F.
 let c = p.constructor; // This is the function associated with the prototype.
 console.log(c === F);                // => true: F.prototype.constructor === F for any F
+
+let o = new F();      // Create an object o of class F
+console.log(o.constructor === F);   // => true: the constructor property specifies the class
