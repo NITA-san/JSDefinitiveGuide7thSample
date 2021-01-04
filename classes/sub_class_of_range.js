28605ad9176fddfb61ac03a4f9898359ba9084b7
NITA-san <ysnitta@gmail.com>
2021-01-04 21:53:34 +0900

    Try sub class of range

---
+++ classes/sub_class_of_range.js
@@ -0,0 +1,47 @@
+class Range {
+    constructor(from, to) {
+        // Store the start and end points (state) of this new range object.
+        // These are noninherited properties that are unique to this object.
+        this.from = from;
+        this.to = to;
+    }
+
+    // Return true if x is in the range, false otherwise
+    // This method works for textual and Date ranges as well as numeric.
+    includes(x) { return this.from <= x && x <= this.to; }
+
+    // A generator function that makes instances of the class iterable.
+    // Note that it only works for numeric ranges.
+    *[Symbol.iterator]() {
+        for(let x = Math.ceil(this.from); x <= this.to; x++) yield x;
+    }
+
+    // Return a string representation of the range
+    toString() { return `(${this.from}...${this.to})`; }
+}
+
+// This is the constructor function for our subclass
+function Span(start, span) {
+    if (span >= 0) {
+        this.from = start;
+        this.to = start + span;
+    } else {
+        this.to = start;
+        this.from = start + span;
+    }
+}
+
+// Ensure that the Span prototype inherits from the Range prototype
+Span.prototype = Object.create(Range.prototype);
+
+// We don't want to inherit Range.prototype.constructor, so we
+// define our own constructor property.
+Span.prototype.constructor = Span;
+
+// By defining its own toString() method, Span overrides the
+// toString() method that it would otherwise inherit from Range.
+Span.prototype.toString = function() {
+    return `(${this.from}... +${this.to - this.from})`;
+};
+
+console.log(new Span(1,2).toString());
