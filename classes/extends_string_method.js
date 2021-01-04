d92ab80715a3e5527e1bd3d974f2d9c40f4b4026
NITA-san <ysnitta@gmail.com>
2021-01-04 21:39:33 +0900

    Try extend string method

---
+++ classes/extends_string_method.js
@@ -0,0 +1,11 @@
+console.log("test".startsWith("t"));
+
+// If the new String method startsWith() is not already defined...
+if (!String.prototype.startsWith) {
+    // ...then define it like this using the older indexOf() method.
+    String.prototype.startsWith = function(s) {
+        return this.indexOf(s) === 0;
+    };
+}
+
+console.log("test".startsWith("t"));
