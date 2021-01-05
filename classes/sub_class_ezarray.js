27b6a3e4fb17977f5d62465f14d5b6128c747bda
NITA-san <ysnitta@gmail.com>
2021-01-05 23:49:21 +0900

    Check ezarray prototype feature

--- classes/sub_class_ezarray.js
+++ classes/sub_class_ezarray.js
@@ -14,3 +14,12 @@ a.last                // => 3: last getter defined by subclass
 a[1]                  // => 2: regular array access syntax still works.
 Array.isArray(a)      // => true: subclass instance really is an array
 console.log(EZArray.isArray(a));    // => true: subclass inherits static methods, too!
+
+// EZArray inherits instance methods because EZArray.prototype
+// inherits from Array.prototype
+console.log(Array.prototype.isPrototypeOf(EZArray.prototype)); // => true
+
+// And EZArray inherits static methods and properties because
+// EZArray inherits from Array. This is a special feature of the
+// extends keyword and is not possible before ES6.
+console.log(Array.isPrototypeOf(EZArray)); // => true
