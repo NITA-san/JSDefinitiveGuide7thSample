a84aabd86658d6deacaa927f512c2cffd53e98d4
NITA-san <ysnitta@gmail.com>
2021-01-08 09:35:53 +0900

    Check Object create method

--- objects/object_creation.js
+++ objects/object_creation.js
@@ -4,3 +4,9 @@ let o3 = Object.create(Object.prototype);
 let o = { x: "don't change this value" };
 //library.function(Object.create(o));  // Guard against accidental modifications
 
+let o2 = Object.create(o);
+console.log(o===o2);
+console.log(o);
+console.log(o2);
+console.log(o.x);
+console.log(o2.x);
