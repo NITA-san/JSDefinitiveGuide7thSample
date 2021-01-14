b50778a48f2e0c8d940cc7e8061043ae2441a95e
NITA-san <ysnitta@gmail.com>
2021-01-14 21:41:26 +0900

    Try set behavior

--- standard-library/sets.js
+++ standard-library/sets.js
@@ -23,3 +23,9 @@ console.log(oneDigitPrimes.has(2));    // => true: 2 is a one-digit prime number
 console.log(oneDigitPrimes.has(3));    // => true: so is 3
 console.log(oneDigitPrimes.has(4));    // => false: 4 is not a prime
 console.log(oneDigitPrimes.has("5"));  // => false: "5" is not even a number
+
+let sum = 0;
+for(let p of oneDigitPrimes) { // Loop through the one-digit primes
+    sum += p;                  // and add them up
+}
+console.log(sum);                            // => 17: 2 + 3 + 5 + 7
