beafa056a54e2d2db6d929c4f3d59dc455112dbe
NITA-san <ysnitta@gmail.com>
2021-01-14 21:40:41 +0900

    Try set behavior

---
+++ standard-library/sets.js
@@ -0,0 +1,25 @@
+let s = new Set();       // A new, empty set
+let t = new Set([1, s]); // A new set with two members
+
+console.log(s);
+console.log(t);
+
+let u = new Set(t);                  // A new set that copies the elements of s.
+let unique = new Set("Mississippi"); // 4 elements: "M", "i", "s", and "p"
+
+console.log(u);
+console.log(unique);
+
+let a = new Set();
+console.log(a.add([1,2]));
+console.log(a.add([1,2]));
+console.log(a.delete([1,2]));
+
+console.log('------');
+
+
+let oneDigitPrimes = new Set([2,3,5,7]);
+console.log(oneDigitPrimes.has(2));    // => true: 2 is a one-digit prime number
+console.log(oneDigitPrimes.has(3));    // => true: so is 3
+console.log(oneDigitPrimes.has(4));    // => false: 4 is not a prime
+console.log(oneDigitPrimes.has("5"));  // => false: "5" is not even a number
