bc4d963cf5a81394ad6d490b0baa2b4d5148a197
NITA-san <ysnitta@gmail.com>
2021-01-20 22:30:57 +0900

    Try regexp match named capture

--- standard-library/regexp_match.js
+++ standard-library/regexp_match.js
@@ -1,3 +1,32 @@
-let match = "7 plus 8 equals 15".match(/\d+/g)  // => ["7", "8", "15"]
+let matched = "7 plus 8 equals 15".match(/\d+/g)  // => ["7", "8", "15"]
 
-console.log(match);
+console.log(matched);
+
+// A very simple URL parsing RegExp
+let url = /(\w+):\/\/([\w.]+)\/(\S*)/;
+let text = "Visit my blog at http://www.example.com/~david";
+let match = text.match(url);
+let fullurl, protocol, host, path;
+if (match !== null) {
+    fullurl = match[0];   // fullurl == "http://www.example.com/~david"
+    protocol = match[1];  // protocol == "http"
+    host = match[2];      // host == "www.example.com"
+    path = match[3];      // path == "~david"
+}
+
+console.log(fullurl);
+console.log(protocol);
+console.log(host);
+console.log(path);
+
+console.log('----------')
+
+url = /(?<protocol>\w+):\/\/(?<host>[\w.]+)\/(?<path>\S*)/;
+text = "Visit my blog at http://www.example.com/~david";
+match = text.match(url);
+console.log(match[0]);               // => "http://www.example.com/~david"
+console.log(match.input);            // => text
+console.log(match.index);            // => 17
+console.log(match.groups.protocol);  // => "http"
+console.log(match.groups.host);      // => "www.example.com"
+console.log(match.groups.path);      // => "~david"
