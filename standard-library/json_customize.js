9c275f8f8baae52179da25b9dd2406154482b4ad
NITA-san <ysnitta@gmail.com>
2021-01-23 14:39:48 +0900

    Try json customize

---
+++ standard-library/json_customize.js
@@ -0,0 +1,17 @@
+let text = JSON.stringify({a:"2021-01-23T14:38:24.222Z"});
+
+let data = JSON.parse(text, function(key, value) {
+    // Remove any values whose property name begins with an underscore
+    if (key[0] === "_") return undefined;
+
+    // If the value is a string in ISO 8601 date format convert it to a Date.
+    if (typeof value === "string" &&
+        /^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ$/.test(value)) {
+        return new Date(value);
+    }
+
+    // Otherwise, return the value unchanged
+    return value;
+});
+
+console.log(data);
