0f1191265dd055ff0891ffffb076056a6a579532
NITA-san <ysnitta@gmail.com>
2021-02-01 22:42:47 +0900

    Try seal behavior

--- metaprogramming/object_seal.js
+++ metaprogramming/object_seal.js
@@ -1,3 +1,11 @@
 // Create a sealed object with a frozen prototype and a non-enumerable property
-let o = Object.seal(Object.create(Object.freeze({x: 1}), {y: {value: 2, writable: true}}));
-console.log(o);
+let q = Object.freeze({x:1});
+q.a = 2;
+console.log(q);
+let p = Object.create(Object.freeze({x: 1}), {y: {value: 2, writable: true}});
+console.log(p);
+let o = Object.seal(p);
+console.log(o.x);
+console.log(o.y);
+o.x=3;
+console.log(o.x);
