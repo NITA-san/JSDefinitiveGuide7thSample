4e7a5de45635cb4bf40c1f4a525504ff0fc4e653
NITA-san <ysnitta@gmail.com>
2021-02-07 16:34:47 +0900

    Try non spreadable array

--- metaprogramming/arraylike.js
+++ metaprogramming/arraylike.js
@@ -5,3 +5,9 @@ let arraylike = {
 };
 console.log(arraylike);
 console.log([].concat(arraylike));  // => [1]: (would be [[1]] if not spread)
+
+class NonSpreadableArray extends Array {
+    get [Symbol.isConcatSpreadable]() { return false; }
+}
+let a = new NonSpreadableArray(1,2,3);
+console.log([].concat(a).length); // => 1; (would be 3 elements long if a was spread)
