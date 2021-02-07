cd6c92eb0462772b7f3d77441b8026338bd250db
NITA-san <ysnitta@gmail.com>
2021-02-07 18:45:51 +0900

    Try unscopable behavior

---
+++ metaprogramming/unscopable.js
@@ -0,0 +1,15 @@
+let newArrayMethods = Object.keys(Array.prototype[Symbol.unscopables]);
+
+const object1 = {
+    property1: 42
+  };
+  
+  object1[Symbol.unscopables] = {
+    property1: true
+  };
+  
+  with (object1) {
+    console.log(property1);
+    // expected output: Error: property1 is not defined
+  }
+  
