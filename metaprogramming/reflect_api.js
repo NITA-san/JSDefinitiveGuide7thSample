48c64d1a706357b8d425d1dd393294e5c01312f2
NITA-san <ysnitta@gmail.com>
2021-02-10 22:27:09 +0900

    Check reflect getprototypeof

--- metaprogramming/reflect_api.js
+++ metaprogramming/reflect_api.js
@@ -9,7 +9,11 @@ let p = {
     id: "pid"
 };
 
+let arr = /^(.*)/;
+
 console.log(o.get);
 console.log(Reflect.get(o,"get",p));
 
 console.log(Reflect.getOwnPropertyDescriptor(p, "id"));
+console.log(Reflect.getPrototypeOf(arr));
+console.log(arr);
